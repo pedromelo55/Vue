3e3380beca63ea75b5bccf75ba09ac3b132e64b3
Pedro Melo <pedromelo55@gmail.com>
2023-05-18 10:38:45 -0300

    branch created

--- components/ServidorCard.vue
+++ components/ServidorCard.vue
@@ -1,9 +1,11 @@
 <template>  
     <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <div class="flex justify-end px-4 pt-4">
-            <button id="dropdownButton" data-dropdown-toggle="dropdown" class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
+            <button id="dropdownButton" data-dropdown-toggle="dropdown" class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100
+            dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
                 <span class="sr-only">Open dropdown</span>
-                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path></svg>
+                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M6
+                    10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path></svg>
             </button>
             <!-- Dropdown menu -->
             <div id="dropdown" class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
@@ -21,19 +23,36 @@
             </div>
         </div>
         <div class="flex flex-col items-center pb-10">
-            <img class="w-24 h-28 mb-3 rounded-full shadow-lg" src="https://sgi.desenvolvimento.go.gov.br/perfil/70751058173.jpg" alt="Bonnie image"/>
-            <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Pedro Melo</h5>
-            <h6 class="mb-1 text-l font-medium text-gray-600 dark:text-white">Gerência de Tecnologia</h6>
-            <span class="text-sm text-gray-500 dark:text-gray-400">Estagiário</span>
+            <!-- <img class="w-24 h-28 mb-3 rounded-full shadow-lg" :src="'https://sgi.desenvolvimento.go.gov.br/perfil/' + servidor.cpf + '.jpg'" alt="Imagem do servidor"/> -->
+            <img id="avatar-profile" class="w-24 h-28 mb-3 rounded-full shadow-lg" :src="'https://sgi.desenvolvimento.go.gov.br/perfil/'
+                + servidor.cpf + '.jpg'" alt="Imagem do servidor"
+                onerror="this.onerror=null; this.src='https://sgi.desenvolvimento.go.gov.br/perfil/0.png'"
+            />
+            <h5 class="mb-1 text-center text-xl font-medium text-gray-900 dark:text-white">{{ servidor.nome }}</h5>
+            <h6 class="mb-1 text-center text-l font-medium text-gray-600 dark:text-white">{{ servidor.lotacao.unidade }}</h6>
+            <!--<span class="text-sm text-gray-500 dark:text-gray-400">Talvez colocar cargo</span>-->
             <div class="flex mt-4 space-x-3 md:mt-6">
-                <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a>
-                <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Message</a>
+                <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-goias-50
+                rounded-lg hover:bg-goias-100 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700
+                dark:focus:ring-blue-800">Editar</a>
+                <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border
+                border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800
+                dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Visualizar</a>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
+import { initFlowbite } from 'flowbite'
+const props = defineProps(['servidor'])
+
+ 
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
 
 </script>
 
--- pages/servidores.vue
+++ pages/servidores.vue
@@ -1,25 +1,78 @@
 <template>
-    <div>
-        <h2>Servidores</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam debitis,
-        ipsum expedita doloremque modi incidunt itaque quis asperiores consequatur repellat.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam debitis,
-        ipsum expedita doloremque modi incidunt itaque quis asperiores consequatur repellat.</p>
-        <div>{{ data }}</div>
-        <ServidorCard></ServidorCard>
-    </div>
+  <div>
+      <h2>Servidores</h2>
+      <p>Texto aleatório em português</p>
+      <div class="grid grid-cols-4 gap-5 items-stretch">
+        <ServidorCard v-for="(item, i) in servidores" :key="i" :servidor="item"/>
+      </div>
+      <div class="flex flex-row gap-1">
+        <Pagination  @changePage="change(p)" :pagina="pagina" :page="pages"/>
+      </div>
+      
+      <div class="flex flex-col items-center">
+        <!-- Help text -->
+        <span class="text-sm text-gray-700 dark:text-gray-400">
+            Exibindo <span class="font-semibold text-gray-900 dark:text-white">1</span> até <span class="font-semibold text-gray-900 dark:text-white">20</span> dos <span class="font-semibold text-gray-900 dark:text-white">100</span> Servidores
+        </span>
+        <!-- Buttons -->
+        <div class="inline-flex mt-2 xs:mt-0 gap-2">
+            <button class="px-4 py-2 text-sm font-medium text-white bg-goias-50 rounded-l hover:bg-goias-100 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                Anterior
+            </button>
+            <button class="px-4 py-2 text-sm font-medium text-white bg-goias-50 rounded-r hover:bg-goias-100 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                Próximo
+            </button>
+        </div>
+      </div>
+
+  </div>
 </template>
 
 <script setup>
-    const { data } = await useFetch('/api/currency/GBP')
+// import Pagination from '~/components/pagination.vue';
+
+const { getItems } = useDirectusItems();
+
+const pagina = ref(1)
+
+const fetchArticles = async () => {
+  try {
+    const filters = { content: "testcontent", title: "Test1" };
+    const items = await getItems({
+      collection: "servidores",
+      params: {
+              // filter: filters,
+              fields: [
+                  "*.*"
+              ],
+              limit : 20,
+              page : pagina.value
+          },
+    });
+    return items
+  } catch (e) {}
+};
+
+const servidores = await fetchArticles();
+
+const pages = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+
 </script>
 
 <style scoped>
-    h2 {
-        margin-bottom: 20px;
-        font-size: 36px;
-    }
-    p {
-        margin: 20px 0;
-    }
+  h2 {
+      margin-bottom: 20px;
+      font-size: 36px;
+  }
+  p {
+      margin: 20px 0;
+  }
 </style>
+
+<!-- colocar uma busca na tela -->
+<!-- deixar uma opção para editar o cargo, unidade  -->
+<!-- no organograma um jeito de (interface) definir o responsável pela unidade -->
+<!-- colocar mais páginas em servidores para mostar todo mundo -->
+
+<!-- emit pega dados e joga para fora emitindo um evento -->
+<!-- props e para exportar dados para outro componente -->
