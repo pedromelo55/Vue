d2e7acb4bfbe33a16d0250a0f85b4be4e5cfba1f
pedrolunasr <pedro.luna@outlook.com>
2023-05-22 16:59:14 -0300

    paginacao funcional

--- pages/servidores.vue
+++ pages/servidores.vue
@@ -6,7 +6,7 @@
         <ServidorCard v-for="(item, i) in servidores" :key="i" :servidor="item"/>
       </div>
       <div class="flex flex-row gap-1">
-        <Pagination  @changePage="newPage" :pagina="pagina" :page="pages"/>
+        <Pagination  @changePage="" :pagina="pagina" :page="pages"/>
       </div>
       
       <div class="flex flex-col items-center">
@@ -17,11 +17,11 @@
         <!-- Buttons -->
         <div class="inline-flex mt-2 xs:mt-0 gap-2">
             <button class="px-4 py-2 text-sm font-medium text-white bg-goias-50 rounded-l hover:bg-goias-100 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            >
+            @click="anterior">
                 Anterior
             </button>
             <button class="px-4 py-2 text-sm font-medium text-white bg-goias-50 rounded-r hover:bg-goias-100 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            @click="teste">
+            @click="proximo">
                 Proximo 
             </button>
         </div>
@@ -30,41 +30,66 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 // import Pagination from '~/components/pagination.vue';
 
-const { getItems } = useDirectusItems();
-
-
-
 const pagina = ref(1)
+const limite = ref(20)
+const servidores = ref({}) // corrigir o nome
 
-const fetchArticles = async () => {
-  try {
-    const filters = { content: "testcontent", title: "Test1" };
-    const items = await getItems({
+
+
+// const { getItems } = useDirectusItems();
+
+// const fetchArticles = async () => {
+//   try {
+//     const filters = { content: "testcontent", title: "Test1" };
+//     const items = await getItems({
+//       collection: "servidores",
+//       params: {
+//               // filter: {nome: "Frederico dos Santos Arraes"},
+//               fields: [
+//                   "*.*"
+//               ],
+//               limit : limite.value,
+//               page : pagina.value
+//           },
+//     });
+//     return items
+//   } catch (e) {}
+// };
+// const servidores = await fetchArticles();
+async function carregaServidores(numPagina: number) {
+  useDirectusItems().getItems({
       collection: "servidores",
       params: {
-              // filter: {nome: "Frederico dos Santos Arraes"},
-              fields: [
-                  "*.*"
-              ],
-              limit : 20,
-              page : pagina.value
-          },
-    });
-    return items
-  } catch (e) {}
-};
+        limit: 20,
+        page: numPagina,
+        sort: "nome"
+      },
+    }).then((data) => servidores.value = data);
+}
 
-const servidores = await fetchArticles();
-
+watch(
+  pagina,(newValue)=>carregaServidores(newValue)
+  ,{ immediate: true }
+)
 const pages = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 
-
-function teste(){
-  pagina = ref(2)
+function anterior(){
+  if(pagina.value > 1){
+    pagina.value--
+  }
 }
+
+
+function proximo(){
+    if(pagina.value < 13){
+      pagina.value++
+    }
+  }
+
+
 
 </script>
 
